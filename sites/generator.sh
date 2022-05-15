f0980d2975e4f273969ae90a406e6e29613678b8
andres <andres857@gmail.com>
2022-05-15 21:41:53 +0000

    add change to file nginx

--- sites/generator.sh
+++ sites/generator.sh
@@ -21,6 +21,8 @@ version: '3'
 services: 
 
     nginx:
+        depends_on:
+            - wordpress
         image: nginx:1.21.6-alpine
         restart: unless-stopped
         volumes:
@@ -32,8 +34,8 @@ services:
             - LETSENCRYPT_EMAIL=info@windowschannel.com
         links:
             - wordpress
-        expose: 
-            - "80"
+        expose:
+            - 80
 
     wordpress: 
         image: wordpress:php8.1-fpm-alpine
@@ -47,22 +49,49 @@ services:
 
 EOT
 
-cat <<EOT >> ./$wpdomain/nginx/conf.d/defautl.conf
-server {  
-	listen 80;  
-	listen [::]:80;  
-	access_log off;  
-	root /var/www/html;  
-	index index.php index.html;  
-	server_tokens off;  
+cat <<EOT >> ./$wpdomain/nginx/conf.d/nginx.conf
+server {
+        listen 80;
+        listen [::]:80;
 
-	location ~ \.php$ {
-		include snippets/fastcgi-php.conf;
-		# With php-fpm (or other unix sockets):
-		fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
-		# With php-cgi (or other tcp sockets):
-		#fastcgi_pass 127.0.0.1:9000;
-	}
+        server_name $wpdomain;
 
-	}
+        index index.php index.html index.htm;
+
+        root /var/www/html;
+
+        location ~ /.well-known/acme-challenge {
+                allow all;
+                root /var/www/html;
+        }
+
+        location / {
+                try_files $uri $uri/ /index.php$is_args$args;
+        }
+
+        location ~ \.php$ {
+                try_files $uri =404;
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                fastcgi_pass wordpress:9000;
+                fastcgi_index index.php;
+                include fastcgi_params;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_param PATH_INFO $fastcgi_path_info;
+        }
+
+        location ~ /\.ht {
+                deny all;
+        }
+
+        location = /favicon.ico {
+                log_not_found off; access_log off;
+        }
+        location = /robots.txt {
+                log_not_found off; access_log off; allow all;
+        }
+        location ~* \.(css|gif|ico|jpeg|jpg|js|png)$ {
+                expires max;
+                log_not_found off;
+        }
+}
 EOT
